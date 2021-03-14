d6b0830e4367400849e2dc4ff4a84e1355c80994
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2021-03-14 21:06:01 +0900

    3

---
+++ book/2/3.ml
@@ -0,0 +1,14 @@
+let pi = 3.1415
+
+;;
+pi
+
+let area_circle = 2. *. 2. *. pi
+
+let one = 1
+
+let two = one + one
+
+let one = "One"
+
+let three = one ^ one ^ one
---
+++ book/2/3p.ml
@@ -0,0 +1,7 @@
+let _ = "hoge"
+
+let ___ = "hoge"
+
+let a_2' = 1
+
+let camelCase = "k"
