2ebba021764349e4403ab4e42d42416693d3d931
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2021-03-14 20:53:30 +0900

    prac

---
+++ book/2/1.ml
@@ -0,0 +1,18 @@
+(* これはコメント *)
+
+;;
+abs (3 - 5)
+
+;;
+succ 0
+
+;;
+succ 1
+
+;;
+succ 2
+
+let succ_impl x = x + 1
+
+;;
+succ_impl 0
---
+++ book/2/2p.ml
@@ -0,0 +1,44 @@
+;;
+1
+
+;;
+-2 + 3
+
+;;
+9 / -4
+
+;;
+3 + 5
+
+;;
+float_of_int 3 +. 2.5
+
+;;
+int_of_float 0.7
+
+;;
+char_of_int (int_of_char 'A' + 25)
+
+;;
+int_of_string "0xff"
+
+;;
+5.0 ** 2.0
+
+;;
+8 * -2
+
+;;
+int_of_string "0xff"
+
+;;
+int_of_float (-0.7)
+
+;;
+float_of_int (int_of_float 5.0)
+
+;;
+(sin (3.14 /. 2.) ** 2.) +. (cos (3.14 /. 2.) ** 2.)
+
+;;
+int_of_float (sqrt ((3. *. 3.) +. (4. *. 4.)))
