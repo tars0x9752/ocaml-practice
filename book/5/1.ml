0ee3d300451a83900268a118fb601159eeb29873
tars0x9752 <46079709+tars0x9752@users.noreply.github.com>
2021-04-11 12:19:36 +0900

    5-1

---
+++ book/5/1.ml
@@ -0,0 +1,15 @@
+let _ = [ 3; 9; 0; -10 ]
+
+let week = [ "Sun"; "Mon"; "Tue"; "Wed"; "Thu"; "Fri"; "Sat" ]
+
+let oddnums = [ 3; 9; 253 ]
+
+let more_oddnums = 99 :: oddnums
+
+let evennums = [ 4; 10; 256; 12 ]
+
+let _ = []
+
+let _ = (::)(1, [2])
+
+(* (::)(1, 2) はエラー *)
